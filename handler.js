7d0b836ab8573e1023777f79f19caf6bd0cc6aef
sophie61089 <44974945+sophie61089@users.noreply.github.com>
2019-09-10 13:07:35 +0100

    Insert query working

--- handler.js
+++ handler.js
@@ -21,68 +21,55 @@ module.exports.getSummary = (event, context, callback) => {
 
 module.exports.createUpdateQuery = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
-    const query_name = event.query_name;
-    const query_string = event.query_string;
-    if (event.query_type) {
-        const query_type = event.query_type;
-        const sns_threshold = event.sns_threshold;
-        const result = db.query(
-            `INSERT INTO query_table(query_name, query_string, query_type, sns_threshold) `+
-            `VALUES ('${query_name}', '${query_string}', '${query_type}', ${sns_threshold})`)
-            .then(res => {
-                callback(null, {
-                    statusCode: 200,
-                    body: JSON.stringify(res)
-                })
+    let body = JSON.parse(event.body);
+    const query_name = body.query_name;
+    const query_string = body.query_string;
+    const query_type = body.query_type;
+    const sns_threshold = body.sns_threshold;
+    const final_query = 
+    `INSERT INTO query_table(query_name, query_string, query_type, sns_threshold) `+
+    `VALUES ('${query_name}', '${query_string}', '${query_type}', ${sns_threshold}) `+
+    `ON CONFLICT (query_name) DO UPDATE `+
+    `SET (query_string, query_type, sns_threshold) = `+
+    `ROW('${query_string}', '${query_type}', ${sns_threshold}) WHERE query_table.query_name = '${query_name}';`
+    const result = db.query(final_query)
+        .then(res => {
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify(body)
             })
-            .catch(e => {
-                console.log(e);
-                callback(null, {
-                    statusCode: e.statusCode || 500,
-                    body: 'Error in getSummary: ' + e
-                })
+        })
+        .catch(e => {
+            console.log(e);
+            callback(null, {
+                statusCode: e.statusCode || 500,
+                body: 'Error in getSummary: ' + e
             })
-    } else {
-        const result = db.query(
-            `UPDATE query_table SET (query_string) = ROW('${query_string}') WHERE query_name = '${query_name}';`)
-            .then(res => {
-                callback(null, {
-                    statusCode: 200,
-                    body: JSON.stringify(res)
-                })
-            })
-            .catch(e => {
-                console.log(e);
-                callback(null, {
-                    statusCode: e.statusCode || 500,
-                    body: 'Error in getSummary: ' + e
-                })
-            })
-    }
+        })
+    
 }
 
 module.exports.generateSummary = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
-    const query_name = event.query_name;
-    const query_string = event.query_string;
-    if (event.query_type) {
-        const query_type = event.query_type;
-        const sns_threshold = event.sns_threshold;
-        const result = db.query(
-            `INSERT INTO query_table(query_name, query_string, query_type, sns_threshold) `+
-            `VALUES ('${query_name}', '${query_string}', '${query_type}', ${sns_threshold})`)
-            .then(res => {
-                callback(null, {
-                    statusCode: 200,
-                    body: JSON.stringify(res)
-                })
+    const queries = db.query(
+        `select query_string from query_table where query_type = 'summary_generator';`
+    )
+        .then(res => {
+            
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify(res)
             })
-            .catch(e => {
-                console.log(e);
-                callback(null, {
-                    statusCode: e.statusCode || 500,
-                    body: 'Error in getSummary: ' + e
-                })
+        }).catch(e => {
+            console.log(e);
+            callback(null, {
+                statusCode: e.statusCode || 500,
+                body: 'Error in getSummary: ' + e
             })
-    }
+        })
+    // const lenQueries = queries.length
+    // for (i = 0; i < lenQueries; i++) {
+    //     db.query(query_string)
+    // };
+    // const message = 'Success';
 }
