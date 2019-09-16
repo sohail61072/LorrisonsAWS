6a19aebee422de62ddd14c875e1d9ca6c390d904
sophie61089 <44974945+sophie61089@users.noreply.github.com>
2019-09-16 13:26:42 +0100

    SNS added

--- handler.js
+++ handler.js
@@ -1,5 +1,6 @@
 const db = require('./db_connect');
 var AWS = require("aws-sdk");
+const sns = new AWS.SNS({ region: 'eu-west-2' })
 
 module.exports.getSummary = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
@@ -165,24 +166,33 @@ module.exports.generateSummary = (event, context, callback) => {
                 console.info(`for ${obj.query_name} obj sns_threshold =  ${obj.sns_threshold}, count = ${count}`)
                 if (obj.sns_threshold != null) {
                     console.info(`sns_threshold not null for ${obj.query_name}`)
-                    if (count > obj.sns_threshold) {
-                        console.info(`count is bigger than threshold for ${obj.query_name}`)
-                            var eventText = 'Here is some Text';
-                            var sns = new AWS.SNS();
-                            var params = {
-                                Message: eventText, 
-                                Subject: "Test SNS From Lambda",
-                                TopicArn: "arn:aws:sns:eu-west-2:503104246251:AWSMonitoringMock"
-                            };
-                            sns.publish(params, function(err,data) {
-                                if(err) {
-                                    console.error('error publishing to SNS');
-                                    context.fail(err);
-                                } else {
-                                    console.info('message published to SNS');
-                                    context.succeed(null, data);
-                                }
-                            });
+                    if (4 > obj.sns_threshold) {
+                        try {
+                            const metadata = publishSnsTopic(`INSERT SNS MESSAGE HERE`, `SNS Subject`)
+                            return generateResponse(200, {
+                                message: 'Successfully added the calculation.',
+                                data: metadata
+                            })
+                        } catch (err) {
+                            return generateError(500, new Error('Couldn\'t add the calculation due to an internal error.'))
+                        }
+                        // console.info(`count is bigger than threshold for ${obj.query_name}`)
+                        //     var eventText = 'Here is some Text';
+                        //     var sns = new AWS.SNS();
+                        //     var params = {
+                        //         Message: eventText, 
+                        //         Subject: "Test SNS From Lambda",
+                        //         TopicArn: "arn:aws:sns:eu-west-2:503104246251:AWSMonitoringMock"
+                        //     };
+                        //     sns.publish(params, function(err,data) {
+                        //         if(err) {
+                        //             console.error('error publishing to SNS');
+                        //             context.fail(err);
+                        //         } else {
+                        //             console.info('message published to SNS');
+                        //             context.succeed(null, data);
+                        //         }
+                        //     });
                     } else {
                         console.info(`count not bigger than threshold for ${obj.query_name}`)
                     }
@@ -204,3 +214,26 @@ module.exports.generateSummary = (event, context, callback) => {
         })
     return context.logStreamName;
 }
+
+async function publishSnsTopic (message, subject) {
+    const params = {
+      Message: message,
+      Subject: subject,
+      TopicArn: `arn:aws:sns:eu-west-2:503104246251:AWSMonitoringMock`
+    }
+    return sns.publish(params).promise()
+  }
+
+  function generateResponse (code, payload) {
+    console.log(payload)
+    return {
+      statusCode: code,
+      body: JSON.stringify(payload)
+    }
+  }
+  function generateError (code, err) {
+    console.error(err)
+    return generateResponse(code, {
+      message: err.message
+    })
+  }
